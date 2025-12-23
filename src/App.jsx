b2d2e25bd3a505e9fb80ff9d9bf17be8b28b96b4
Towhidur-Rahmaan <shaontalukdar0@gmail.com>
2025-12-23 12:03:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-amber-700'>Knowledge Cafe</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
